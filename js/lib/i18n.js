c2124642e032c67ca10d1fd6177ebb16ee6acc6b
Nicolas Joyard <joyard.nicolas@gmail.com>
2011-09-08 14:19:55 +0200

    Update to RequireJS 0.26 and add text plugin

--- js/lib/i18n.js
+++ js/lib/i18n.js
@@ -1,7 +1,7 @@
 /*
- RequireJS i18n 0.24.0 Copyright (c) 2010-2011, The Dojo Foundation All Rights Reserved.
+ RequireJS i18n 0.26.0 Copyright (c) 2010-2011, The Dojo Foundation All Rights Reserved.
  Available via the MIT or new BSD license.
  see: http://github.com/jrburke/requirejs for details
 */
-(function(){function r(b,a,i,d,c,e){if(a[b]){i.push(b);if(a[b]===true||a[b]===1)d.push(c+b+"/"+e)}}function s(b,a,i,d,c){a=d+a+"/"+c;require._fileExists(b.nameToUrl(a,null))&&i.push(a)}var u=/(^.*(^|\/)nls(\/|$))([^\/]*)\/?([^\/]*)/;define({version:"0.24.0",load:function(b,a,i,d){d=d||{};var c=u.exec(b),e=c[1],o=c[4],f=c[5],p=o.split("-"),g=[],t={},j,h,k="";if(c[5]){e=c[1];b=e+f}else{b=b;f=c[4];o=d.locale||(d.locale=typeof navigator==="undefined"?"root":(navigator.language||navigator.userLanguage||
-"root").toLowerCase());p=o.split("-")}if(d.isBuild){g.push(b);s(a,"root",g,e,f);for(j=0;h=p[j];j++){k+=(k?"-":"")+h;s(a,k,g,e,f)}a(g);i()}else a([b],function(q){var m=[];r("root",q,m,g,e,f);for(j=0;h=p[j];j++){k+=(k?"-":"")+h;r(k,q,m,g,e,f)}a(g,function(){var n,l;for(n=m.length-1;n>-1&&(h=m[n]);n--){l=q[h];if(l===true||l===1)l=a(e+h+"/"+f);require.mixin(t,l)}i(t)})})}})})();
+(function(){function n(a,b,k,d,c,g){b[a]&&(k.push(a),(b[a]===!0||b[a]===1)&&d.push(c+a+"/"+g))}function o(a,b,k,d,c){b=d+b+"/"+c;require._fileExists(a.toUrl(b))&&k.push(b)}var q=/(^.*(^|\/)nls(\/|$))([^\/]*)\/?([^\/]*)/,r={};define({version:"0.26.0",load:function(a,b,k,d){var d=d||{},c=q.exec(a),g=c[1],l=c[4],i=c[5],m=l.split("-"),e=[],p={},h,j,f="";c[5]?(g=c[1],a=g+i):(i=c[4],l=d.locale||(d.locale=typeof navigator==="undefined"?"root":(navigator.language||navigator.userLanguage||"root").toLowerCase()),
+m=l.split("-"));if(d.isBuild){e.push(a);o(b,"root",e,g,i);for(h=0;j=m[h];h++)f+=(f?"-":"")+j,o(b,f,e,g,i);b(e);k()}else b([a],function(a){var c=[];n("root",a,c,e,g,i);for(h=0;j=m[h];h++)f+=(f?"-":"")+j,n(f,a,c,e,g,i);b(e,function(){var d,e;for(d=c.length-1;d>-1&&(j=c[d]);d--){e=a[j];if(e===!0||e===1)e=b(g+j+"/"+i);var h=p,f=void 0;for(f in e)!(f in r)&&!(f in h)&&(h[f]=e[f])}k(p)})})}})})();
